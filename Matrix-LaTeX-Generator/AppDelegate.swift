eb4417eb092b5fd9bad1ce5d8f140007a555828f
chenguokai <1415430544@qq.com>
2018-07-03 00:07:45 +0800

    Initial Commit

---
+++ Matrix-LaTeX-Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Matrix-LaTeX-Generator
+//
+//  Created by Xim on 2018/7/3.
+//  Copyright © 2018年 chenguokai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Matrix-LaTeX-Generator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Matrix-LaTeX-Generator
+//
+//  Created by Xim on 2018/7/3.
+//  Copyright © 2018年 chenguokai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
